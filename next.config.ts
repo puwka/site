b6d962f8415cc714f48ab3f005878ee6cb59097c
puwka <epicgames4438@gmail.com>
2025-12-19 18:37:10 +0300

    вцвфац

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "**.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
     ],
     unoptimized: false,
   },
